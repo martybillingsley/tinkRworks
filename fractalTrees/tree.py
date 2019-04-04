75f77467e131debf84f897d160c5591110d1d487
martybillingsley <48107311+martybillingsley@users.noreply.github.com>
2019-04-04 17:01:50 -0500

    Update tree.py

--- fractalTrees/tree.py
+++ fractalTrees/tree.py
@@ -3,6 +3,10 @@ from turtle import *
 # define your drawTree() function here
 
 
+
+
+
+
 # move the turtle to a good starting position
 penup()
 goto(0,-200)
@@ -10,6 +14,8 @@ setheading(90)
 pendown()
 
 # call your drawTree function here
+
+
 
 
 # this line is needed to make the GUI window work properly
