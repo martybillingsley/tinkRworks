e0c39286e200275687e6cfb837cbbd17f88da141
martybillingsley <48107311+martybillingsley@users.noreply.github.com>
2019-07-12 16:55:39 -0500

    Update draw.py

--- turtleDraw/draw.py
+++ turtleDraw/draw.py
@@ -5,3 +5,9 @@ win = turtle.Screen()
 # create a turtle named bob
 alex = turtle.Turtle()
 
+
+
+# put your code above this line
+# this command is needed to make the GUI window work properly 
+# and should always be the last line in the program
+turtle.done()
